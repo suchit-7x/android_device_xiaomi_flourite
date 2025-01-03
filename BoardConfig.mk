dd479e398badbcef33dffb405278155265097205
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:15:40 +0100

    flourite: Add recovery flags
    Change-Id: I8fde10f1a50317950597a63249112e0154897e1a
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,9 @@ TARGET_BOARD_PLATFORM := volcano
 
 # Recovery
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Include the proprietary files BoardConfig.
 include vendor/xiaomi/flourite/BoardConfigVendor.mk
