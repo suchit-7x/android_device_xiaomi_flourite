ce6536b4a78d1fceb4d2b63927df15a20d284ffc
LuK1337 <priv.luk@gmail.com>
2025-01-08 22:02:26 +0100

    flourite:  Import fstab from stock
    Change-Id: Iafff80ce70f2c3ba666c741ccd7ec5f70bac5241

--- device.mk
+++ device.mk
@@ -57,6 +57,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab.qcom.vendor_ramdisk
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore_V3.xml
