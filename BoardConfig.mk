9ce7ba58a4737a7ea8860eb18816974c1c6dbd9a
Niyush-04 <mauryapradip04@gmail.com>
2026-02-06 05:40:54 +0530

    flourite: Add kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,8 @@
 
 DEVICE_PATH := device/xiaomi/flourite
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
@@ -16,9 +18,44 @@ TARGET_CPU_VARIANT_RUNTIME := kryo300
 TARGET_BOOTLOADER_BOARD_NAME := flourite
 TARGET_NO_BOOTLOADER := true
 
+# Boot Image
+BOARD_RAMDISK_USE_LZ4 := true
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_INIT_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
+
+# Device Trees
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+TARGET_NEEDS_DTBOIMAGE := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := Image
+
+BOARD_KERNEL_CMDLINE := \
+    sysctl.kernel.firmware_config.force_sysfs_fallback=1 \
+    swinfo.fingerprint=flourite:$(LINEAGE_VERSION) \
+    mtdoops.fingerprint=flourite:$(LINEAGE_VERSION)
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.load_modules_parallel=true \
+    androidboot.vendor.qspa=true \
+    androidboot.hypervisor.protected_vm.supported=false
+
+BOARD_BOOTCONFIG += \
+    androidboot.selinux=permissive
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := volcano
+
+# Recovery
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 
 # Include the proprietary files BoardConfig.
 include vendor/xiaomi/flourite/BoardConfigVendor.mk
